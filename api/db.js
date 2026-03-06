f8e7c6bef6e01a2d05831aad8790bc1325d7cd16
Rodrigo2612jr <Rpascotojr@gmail.com>
2026-03-06 00:24:54 -0300

    fix: use @libsql/client/web for pure HTTP client in serverless

--- api/db.js
+++ api/db.js
@@ -1,4 +1,4 @@
-const { createClient } = require('@libsql/client');
+﻿const { createClient } = require('@libsql/client/web');
 
 const tursoClient = createClient({
     url: process.env.TURSO_DATABASE_URL,
@@ -121,3 +121,4 @@ module.exports = {
     initDB, getAllClients, getClientById, getClientByName, createClient, updateClient, deleteClient,
     getAllOrders, getOrderById, createOrder, deleteOrder, exportAll, importAll,
 };
+
